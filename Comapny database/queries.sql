7c547c5c2ba1e316d59180b71235661a63932202
Vinayak kapoor <vinayakkapoor678@gmail.com>
2026-01-09 15:46:48 +0530

    Initial SQL mini project

--- Comapny database/queries.sql
+++ Comapny database/queries.sql
@@ -1,6 +1,7 @@
 -- view all employees
 SELECT *
 FROM employees;
+
 -- employees with department
 SELECT e.emp_id,
     e.name,
@@ -8,10 +9,12 @@ SELECT e.emp_id,
     d.dept_name
 FROM employees e
     JOIN departments d ON e.dept_id = d.dept_id;
+
 -- update salary
 UPDATE employees
 SET salary = salary + 5000
 WHERE dept_id = 1;
 -- delete employee
+
 DELETE FROM employees
 WHERE emp_id = 2;
